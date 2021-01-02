d6b00b06da951b55e710074201f2b76dcf66d34f
Aryan Bhatia <aryanbhatia14@gmail.com>
2021-01-02 18:39:10 +1100

    Tests Info: Bug Fixed

--- voice_assistant.py
+++ voice_assistant.py
@@ -10,27 +10,31 @@ scraper = Scraper()
 
 # RegEx Patterns
 BASIC_PATTERNS = {
-    re.compile("how many [\w\s]+ (cases|case)"): scraper.get_cases,
-    re.compile("how many [\w\s]+ (cases|case) worldwide"): scraper.get_cases,
-    re.compile("how many [\w\s]+ (deaths|death|died)"): scraper.get_deaths,
-    re.compile("how many [\w\s]+ (deaths|death|died) worldwide"): scraper.get_deaths,
+    re.compile("[\w\s]+ (cases|case)"): scraper.get_cases,
+    re.compile("[\w\s]+ (cases|case) worldwide"): scraper.get_cases,
+    re.compile("[\w\s]+ (deaths|death)"): scraper.get_deaths,
+    re.compile("[\w\s]+ (deaths|death) worldwide"): scraper.get_deaths,
+}
+
+ACTIVE_CRITICAL_PATTERNS = {
+    re.compile("[\w\s]+ active cases [\w\s]+"): lambda country: scraper.get_active_cases(country),
+    re.compile("[\w\s]+ critical cases [\w\s]+"): lambda country: scraper.get_critical_cases(country),
 }
 
 REGIONAL_PATTERNS = {
     re.compile("[\w\s]+ (cases|case) [\w\s]+"): lambda country: scraper.get_cases(country),
+    re.compile("(cases|case) [\w\s]+"): lambda country: scraper.get_cases(country),
     re.compile("[\w\s]+ (deaths|death|died) [\w\s]+"): lambda country: scraper.get_deaths(country),
+    re.compile("(deaths|death|died) [\w\s]+"): lambda country: scraper.get_deaths(country),
     re.compile("[\w\s]+ recovered [\w\s]+"): lambda country: scraper.get_recovered(country),
-    re.compile("[\w\s]+ (tests|test) [\w\s]+ conducted [\w\s]+"): lambda country: scraper.get_tests_conducted(country),
-}
-
-ACTIVE_CRITICAL_PATTERNS = {
-    re.compile("[\w\s]+ actives (cases|case) [\w\s]+"): lambda country: scraper.get_active_cases(country),
-    re.compile("[\w\s]+ critical (cases|case) [\w\s]+"): lambda country: scraper.get_critical_cases(country),
+    re.compile("[\w\s]+ (tests|test) [\w\s]+"): lambda country: scraper.get_tests_conducted(country),
 }
 
 UPDATE_COMMANDS = ['update', 'update date']
 
 END_PHRASES = ['stop', 'exit']
+
+DEFAULT_RESULT = 'Sorry, I am having trouble fetching that information'
 
 
 def speak(text):
@@ -49,9 +53,21 @@ def get_audio():
             said = r.recognize_google(audio)
 
         except Exception as e:
-            print("Exception: ", str(e))
+            print()
 
     return said.lower()
+
+
+def pattern_checker(text, patterns, result):
+    for pattern, func in patterns.items():
+        if pattern.match(text):
+            words = set(text.split(" "))
+            for country in scraper.get_regions():
+                if country in words:
+                    if result == DEFAULT_RESULT:
+                        result = func(country)
+                    break
+    return result
 
 
 def main():
@@ -61,44 +77,32 @@ def main():
         print("Listening...")
         text = get_audio()
         print(text)
-        result = None
-
-        for pattern, func in BASIC_PATTERNS.items():
-            if pattern.match(text):
-                result = func()
-                break
-
-        for pattern, func in ACTIVE_CRITICAL_PATTERNS.items():
-            if pattern.match(text):
-                words = set(text.split(" "))
-                for country in scraper.get_regions():
-                    if country in words:
-                        result = func(country)
-                        break
-
-        for pattern, func in REGIONAL_PATTERNS.items():
-            if pattern.match(text):
-                words = set(text.split(" "))
-                for country in scraper.get_regions():
-                    if country in words:
-                        result = func(country)
-                        break
-
-        for update_command in UPDATE_COMMANDS:
-            if update_command in text:
-                result = "Data is being updated. This may take a moment!"
-                scraper.update_data()
-                break
-
-        if result:
-            print(result)
-            speak(result)
+        result = DEFAULT_RESULT
 
         for end_phrase in END_PHRASES:
             if end_phrase in text:
                 # stop loop
                 speak('Thank you!')
                 sys.exit('Thank you!')
+
+        result = pattern_checker(text, ACTIVE_CRITICAL_PATTERNS, result)
+        result = pattern_checker(text, REGIONAL_PATTERNS, result)
+
+        for pattern, func in BASIC_PATTERNS.items():
+            if pattern.match(text):
+                if result == DEFAULT_RESULT:
+                    result = func()
+                break
+
+        for update_command in UPDATE_COMMANDS:
+            if update_command in text:
+                if result == DEFAULT_RESULT:
+                    result = "Data is being updated. This may take a moment!"
+                scraper.update_data()
+                break
+
+        print(result)
+        speak(result)
 
 
 main()
